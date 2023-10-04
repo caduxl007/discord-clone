62e3ce6e3108b5dd44e7e6405511489c576d2659
caduxl007 <eduardo2020dev@gmail.com>
2023-10-03 23:28:53 -0300

    feat: add modal delete message

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -11,6 +11,7 @@ import { DeleteServerModal } from '../modals/delete-server-modal'
 import { DeleteChannelModal } from '../modals/delete-channel-modal'
 import { EditChannelModal } from '../modals/edit-channel-modal'
 import { MessageFileModal } from '../modals/message-file-modal'
+import { DeleteMessageModal } from '../modals/delete-message-modal'
 
 export function ModalProvider() {
   useIsMounted()
@@ -27,6 +28,7 @@ export function ModalProvider() {
       <DeleteChannelModal />
       <EditChannelModal />
       <MessageFileModal />
+      <DeleteMessageModal />
     </>
   )
 }
